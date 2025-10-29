3ad49073f92b59751d36d02ab4f3a4b8105c5edf
Yoshikazu <walkingcat.cute@gmail.com>
2025-10-30 01:01:14 +0900

    add: 実行アプリケーション用のプロジェクト追加

---
+++ src/TaskManager.App/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace TaskManager.App;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddWpfBlazorWebView();
+        serviceCollection.AddBlazorWebViewDeveloperTools();
+        Resources.Add("services", serviceCollection.BuildServiceProvider());
+    }
+}
