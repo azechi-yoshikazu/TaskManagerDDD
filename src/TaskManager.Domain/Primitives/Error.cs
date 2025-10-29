68c91eb984213807c84ff3af9215ca56e7f4d717
Yoshikazu <walkingcat.cute@gmail.com>
2025-10-30 03:05:16 +0900

    add: 基本的なエラー、戻り値用のクラスを追加

---
+++ src/TaskManager.Domain/Primitives/Error.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Domain.Primitives
+{
+    public sealed record Error(string Code, string? Message = null)
+    {
+        public static Error None => new Error(string.Empty);
+    }
+}
