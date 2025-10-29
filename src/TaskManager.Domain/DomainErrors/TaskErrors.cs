d932b66a2d12a71d53086616fdd41c8195bd0bbe
Yoshikazu <walkingcat.cute@gmail.com>
2025-10-30 03:36:37 +0900

    clean: コード整理
    ・不要なネームスペースの削除
    ・ファイルスコープネームスペースへの変更

--- src/TaskManager.Domain/DomainErrors/TaskErrors.cs
+++ src/TaskManager.Domain/DomainErrors/TaskErrors.cs
@@ -1,10 +1,9 @@
 ﻿using TaskManager.Domain.Primitives;
 
-namespace TaskManager.Domain.DomainErrors
+namespace TaskManager.Domain.DomainErrors;
+
+public static class TaskErrors
 {
-    public static class TaskErrors
-    {
-        public static Error TitleEmpty => new Error("Task.Title.Empty", "タスクタイトルは空には出来ません。");
-        public static Error TitleTooLong => new Error("Task.Title.TooLong", "タスクタイトルが長すぎます");
-    }
+    public static Error TitleEmpty => new Error("Task.Title.Empty", "タスクタイトルは空には出来ません。");
+    public static Error TitleTooLong => new Error("Task.Title.TooLong", "タスクタイトルが長すぎます");
 }
--- src/TaskManager.Domain/Primitives/Error.cs
+++ src/TaskManager.Domain/Primitives/Error.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TaskManager.Domain.Primitives
+﻿namespace TaskManager.Domain.Primitives
 {
     public sealed record Error(string Code, string? Message = null)
     {
