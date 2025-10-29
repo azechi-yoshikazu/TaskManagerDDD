ccf2c617c74b2d012bf676e5cb328c38318f70cb
Yoshikazu <walkingcat.cute@gmail.com>
2025-10-30 03:07:48 +0900

    fix: エラーメッセージを日本語化

--- src/TaskManager.Domain/DomainErrors/TaskErrors.cs
+++ src/TaskManager.Domain/DomainErrors/TaskErrors.cs
@@ -4,7 +4,7 @@ namespace TaskManager.Domain.DomainErrors
 {
     public static class TaskErrors
     {
-        public static Error TitleEmpty => new Error("Task.Title.Empty", "The task title cannot be empty.");
-        public static Error TitleTooLong => new Error("Task.Title.TooLong", "The task title exceeds the maximum allowed length.");
+        public static Error TitleEmpty => new Error("Task.Title.Empty", "タスクタイトルは空には出来ません。");
+        public static Error TitleTooLong => new Error("Task.Title.TooLong", "タスクタイトルが長すぎます");
     }
 }
