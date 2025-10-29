41104452c7989b274cd1181d4f3fa15119274185
Yoshikazu <walkingcat.cute@gmail.com>
2025-10-30 02:13:58 +0900

    add: DDDの基礎クラスを追加

---
+++ src/TaskManager.Domain/Primitives/Entity.cs
@@ -0,0 +1,23 @@
+﻿namespace TaskManager.Domain.Primitives;
+
+public abstract class Entity
+{
+    public Guid Id { get; private set; }
+
+    protected Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not Entity other)
+            return false;
+        return Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+}
