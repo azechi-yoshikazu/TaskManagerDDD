0632a395795501c0c2d20af16825a98b821bfc67
Yoshikazu <walkingcat.cute@gmail.com>
2025-11-01 01:43:58 +0900

    feat: ユーザー関連のサービスを追加

--- src/TaskManager.Domain/DomainErrors/UserErrors.cs
+++ src/TaskManager.Domain/DomainErrors/UserErrors.cs
@@ -4,9 +4,13 @@ namespace TaskManager.Domain.DomainErrors
 {
     public static class UserErrors
     {
+        public static readonly Error NotFound = new Error("User.NotFound", "ユーザーが存在しません。");
+        public static readonly Error CannotDeleteUserOwningProjects = new Error("User.CannotDeleteUserOwningProjects", "プロジェクトオーナーのユーザーは削除できません。");
+
         public static readonly Error DisplayNameIsRequired = new Error("User.DisplayName.IsRequired", "表示名は必須です。");
 
         public static readonly Error EmailIsRequired = new Error("User.Email.IsRequired", "メールアドレスは必須です。");
         public static readonly Error EmailInvalidFormat = new Error("User.Email.InvalidFormat", "メールアドレスの形式が正しくありません。");
+
     }
 }
